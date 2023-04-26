dc7e0e42035b13f2d1a6b3ea3e532d04538ffd45
sithumigamage <sithumigamage12@gmail.com>
2023-04-26 22:27:07 +0530

    new commit

---
+++ Login_Signup/src/login_signup/Login_Signup.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_signup;
+
+/**
+ *
+ * @author user
+ */
+public class Login_Signup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login LoginFram= new Login();
+         LoginFram.setVisible(true);
+         LoginFram.pack();
+         LoginFram.setLocationRelativeTo(null);//application ope in center in desktop
+         
+    }
+    
+}
